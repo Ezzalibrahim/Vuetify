2b361102a0dfde30036c447494be2f96d29fbbe5
Brahim_ezz <brahim.ezzali@hotmail.com>
2021-01-20 23:57:29 +0100

    add and do the first test of Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-bottom-navigation
+    v-model="value"
+    :background-color="color"
+    dark
+    shift
+  >
+    <v-btn>
+      <span>Video</span>
+      <br>
+      <v-icon>mdi-television-play</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Music</span>
+
+      <v-icon>mdi-music-note</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Book</span>
+
+      <v-icon>mdi-book</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Image</span>
+
+      <v-icon>mdi-image</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
+// import NavBar from './components/NavBar'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    // NavBar
+  },
+
+  data: () => ({ value: 0 }),
+
+  computed: {
+    color () {
+      switch (this.value) {
+        case 0: return 'blue-grey'
+        case 1: return 'teal'
+        case 2: return 'brown'
+        case 3: return 'indigo'
+        default: return 'blue-grey'
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+  .hhh{
+    display: block;
+  }
 </style>
