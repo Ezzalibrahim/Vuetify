4d6347267d9f76b0b8233a0228bb84c5558a063d
unknown <brahim.ezzali@hotmail.com>
2021-01-23 16:16:17 +0100

    Add Header and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,59 @@
 <template>
   <v-app>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title >
-        <h1 class="display-1">Login Form </h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
-          <v-text-field label="Password" :type="showtype" @click:append="ChangeType" prepend-icon="mdi-lock" :append-icon="toggelIcon"></v-text-field>
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="info">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="deep-primary accent-4">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-app-bar app color="info" >
+    <v-toolbar-title> DoctorIno</v-toolbar-title> 
+    <v-spacer></v-spacer>
+    <v-btn text rounded>Home</v-btn> 
+    <v-btn text rounded>Login</v-btn> 
+    <v-btn text rounded>Register</v-btn> 
+  </v-app-bar>
+
+    <!-- Login Form  -->
+    <v-content>
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title >
+          <h1 class="display-1">Login Form </h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
+            <v-text-field label="Password" :type="showtype" @click:append="ChangeType" prepend-icon="mdi-lock" :append-icon="toggelIcon"></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="info">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="deep-primary accent-4">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+    <v-footer
+      color="primary lighten-1"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
@@ -33,6 +70,9 @@ export default {
   data :()=>{
     return{
         showPassword : false,
+        links:[
+          "Home","Login","Register"
+        ]
     }
   },
   computed:{
