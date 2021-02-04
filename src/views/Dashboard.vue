669fef48587f3eed8c608ddb5e4b12915ecf1926
unknown <brahim.ezzali@hotmail.com>
2021-02-04 17:45:55 +0100

    Load More Content on scroll using Intersecting & v-sheet

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -20,6 +20,30 @@
         />
       </v-col>
     </v-row>
+  <!-- Same Statistique  -->
+        <v-row>
+      <v-col
+        cols="6" md="6" lg="3"
+        v-for="statistic in statistics"
+        :key="`${statistic.title}`"
+      >
+        <StatisticCard
+          :statistic="statistic"
+        />
+      </v-col>
+    </v-row>
+  <!-- Same Statistique  -->
+        <v-row>
+      <v-col
+        cols="6" md="6" lg="3"
+        v-for="statistic in statistics"
+        :key="`${statistic.title}`"
+      >
+        <StatisticCard
+          :statistic="statistic"
+        />
+      </v-col>
+    </v-row>
 
     <v-row>
       <v-col cols="12" md="8">
@@ -29,6 +53,29 @@
         <EventTimeline :timeline="timeline" />
       </v-col>
     </v-row>
+    
+    <!-- Same Employees -->
+    <v-row v-intersect="LoadMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-sheet
+        v-if="ShowNewContent"
+        id="moreContent"
+        :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+        class="pa-3 mt-4"
+        >
+        <v-skeleton-loader
+          class="mx-auto"
+          max-width="300"
+          type="card"
+        ></v-skeleton-loader>
+    </v-sheet>
 
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
@@ -61,6 +108,10 @@ export default {
   },
   data() {
     return {
+      theme: {
+        default: { isDark: false },
+      },
+      ShowNewContent:false,
       employees: employeesData,
       loadNewContent: false,
       sales: salesData,
@@ -79,8 +130,9 @@ export default {
       this.selectedEmployee.name = event.name
       this.selectedEmployee.title = event.title
     },
-    showMoreContent(entries) {
-      this.loadNewContent = entries[0].isIntersecting
+    LoadMoreContent(entries) {
+      console.log(entries[0].isIntersecting);
+      this.ShowNewContent = entries[0].isIntersecting;
     }
   }
 }
